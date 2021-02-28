8c9f5416bb359450ff12b37db0910425477c1d8d
Cheng <calvindemeester@gmail.com>
2021-02-28 20:30:33 +0100

    nieuwe product

--- website/pages/menu.php
+++ website/pages/menu.php
@@ -6,6 +6,7 @@
     <div class="row">
       <div class="12u box">
         <iframe id="menuIframe" width="100%" height="1750px" src="images/menukaart-19-12-2020.html"></iframe>
+        <image width="100%" height="auto" src="images/krokantespek.png"/>
       </div>
     </div>
   </div>
